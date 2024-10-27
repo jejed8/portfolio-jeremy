8489a1993e80759442201073da436069c5ff3a43
jejed8 <47800238+jejed8@users.noreply.github.com>
2024-10-26 21:25:41 -0400

    Ajout section projet

--- script.js
+++ script.js
@@ -3,9 +3,23 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
   // gsap code here!
+
+  const scrollContent = document.querySelector('.projects-content');
+  const totalWidth = scrollContent.scrollWidth - window.innerWidth; // Total scrollable width
+  
+  gsap.to(scrollContent, {
+      x: -totalWidth, // Scroll to the end of the content
+      ease: "none",
+      scrollTrigger: {
+          trigger: ".projects",
+          start: "top top", // Start when the top of the section is at the top of the viewport
+          end: () => "+=" + totalWidth, // End when the total scrollable width is reached
+          pin: true, // Pin the section during the scroll
+          scrub: 1, // Smooth scrubbing
+          anticipatePin: 1
+      }
+  });
 });
-
-
 
 const container = document.querySelector(".horizontal-scroll-container");
 
